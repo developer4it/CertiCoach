3f265808d9ae8d177b2493cfe8c38cbab5d4100e
paulkok <paul.kok@it-topdogs.com>
2025-11-21 10:58:55 +0100

    refactoring

---
+++ src/test/java/com/example/cerrticoach/CerrtiCoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cerrticoach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CerrtiCoachApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
